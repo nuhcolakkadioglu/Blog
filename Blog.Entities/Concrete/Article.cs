78241feb3e7cf0e3c3bc43207d2e796469cb5b17
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 11:22:42 +0300

    Comment Entity eklendi ve diğer entitylerde düzenlemeler yapıldı.

--- Blog.Entities/Concrete/Article.cs
+++ Blog.Entities/Concrete/Article.cs
@@ -9,6 +9,7 @@ namespace Blog.Entities.Concrete
 {
     public class Article : EntityBase, IEntity
     {
+
         public string Title { get; set; }
         public string Content { get; set; }
         public string Thumbnail { get; set; }
@@ -22,5 +23,6 @@ namespace Blog.Entities.Concrete
         public Category Category { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
