a04843287eb33eac706847d4faba8de603b49556
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 16:07:44 +0300

    Category ekleme ve güncelleme dto lar tamamlandı.

--- Blog.Entities/Dtos/CategoryUpdateDto.cs
+++ Blog.Entities/Dtos/CategoryUpdateDto.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Entities.Dtos
 {
- public   class CategoryUpdateDto
+    public class CategoryUpdateDto : CategoryAddDto
     {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Silindi mi?")]
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsDeleted { get; set; }
+
     }
 }
