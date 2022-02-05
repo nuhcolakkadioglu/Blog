db1857a6f3950d689683efbd2ab2f33024a38c4e
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 15:31:53 +0300

    IResult eklendi.

---
+++ Blog.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Shared.Utilities.Results.ComplexType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        ResultStatus ResultStatus { get;}
+        string Message { get; }
+        Exception Exception { get; }
+    }
+}
