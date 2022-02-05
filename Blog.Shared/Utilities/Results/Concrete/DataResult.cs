d2e0a87f7ee42dbc25a12df9514002052632b0a2
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 15:42:30 +0300

    DataResult eklendi.

---
+++ Blog.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,40 @@
+﻿using Blog.Shared.Utilities.Results.Abstract;
+using Blog.Shared.Utilities.Results.ComplexType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public T Data { get; }
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+
+
+        public DataResult(ResultStatus resultStatus,T data)
+        {
+            this.Data = data;
+            this.ResultStatus = resultStatus;
+        }
+
+        public DataResult(ResultStatus resultStatus,string message, T data)
+        {
+            this.Data = data;
+            this.Message = message;
+            this.ResultStatus = resultStatus;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data,Exception exception)
+        {
+            this.Data = data;
+            this.Message = message;
+            this.ResultStatus = resultStatus;
+            this.Exception = exception;
+        }
+
+    }
+}
