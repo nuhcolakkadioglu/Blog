d81f009724bd0e11fc159cbcf839d5e815dee953
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 19:43:07 +0300

    ArticleAdd ve Update Dto ları eklendi.

--- Blog.Entities/Dtos/ArticleDtos/ArticleAddDto.cs
+++ Blog.Entities/Dtos/ArticleDtos/ArticleAddDto.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Blog.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,55 @@ namespace Blog.Entities.Dtos.ArticleDtos
 {
     public class ArticleAddDto
     {
+        [DisplayName("Başlık")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MaxLength(100, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string Title { get; set; }
+
+        [DisplayName("İçerik")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MinLength(20, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string Content { get; set; }
+
+        [DisplayName("Thumbnail")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MaxLength(250, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        [MinLength(5, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string Thumbnail { get; set; }
+
+        [DisplayName("Tarih")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("Seo Yazar")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MaxLength(50, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string SeoAuthor { get; set; }
+
+        [DisplayName("Seo Açıklama")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MaxLength(150, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string SeoDescription { get; set; }
+
+        [DisplayName("Seo Etiket")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        [MaxLength(70, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        [MinLength(0, ErrorMessage = "{0} alanı {1} karakterden büyük olamaz")]
+        public string SeoTags { get; set; }
+
+        [DisplayName("Kategori")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [DisplayName("Aktif mi?")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        public bool IsActive { get; set; }
+
 
     }
 }
--- Blog.Entities/Dtos/ArticleDtos/ArticleUpdateDto.cs
+++ Blog.Entities/Dtos/ArticleDtos/ArticleUpdateDto.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Entities.Dtos.ArticleDtos
 {
-    public class ArticleUpdateDto
+    public class ArticleUpdateDto : ArticleAddDto
     {
+        [Required]
+        public int Id { get; set; }
 
+        [DisplayName("Silindi mi?")]
+        [Required(ErrorMessage = "{0} Alanı boş geçilemez")]
+        public bool IsDeleted { get; set; }
     }
 }
