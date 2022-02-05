3299b026a4a5a524c79a7b67816b6f99ab84e874
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 14:12:48 +0300

    RoleMap Eklendi.

---
+++ Blog.Data/Concrete/EntityFramework/Mappings/RoleMap.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Concrete.EntityFramework.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Id).ValueGeneratedOnAdd();
+            builder.Property(m => m.Name).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.Description).IsRequired().HasMaxLength(250);
+
+            builder.Property(m => m.CreatedByName).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.ModifiedByName).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.CreatedDate).IsRequired();
+            builder.Property(m => m.ModifiedDate).IsRequired();
+            builder.Property(m => m.IsActive).IsRequired();
+            builder.Property(m => m.IsDeleted).IsRequired();
+            builder.Property(m => m.Note).HasMaxLength(500);
+
+            builder.ToTable("Roles");
+        }
+    }
+}
