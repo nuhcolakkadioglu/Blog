63ae0b14d4887c116c6f8f9e340ecef87eaf59cf
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-05 13:35:22 +0300

    Context eklendi,  Repository lerde düzenleme

---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Concrete.Repositories
+{
+    public class EfCategoryRepository : EfEntityRepositoryBase<Category>, ICategoryRepository
+    {
+
+        public EfCategoryRepository(DbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ Blog.Data/Concrete/EntityFramework/Repositories/EfRoleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Blog.Data.Abstract;
+using Blog.Entities.Concrete;
+using Blog.Shared.Data.Concrete.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Concrete.Repositories
+{
+    public class EfRoleRepository : EfEntityRepositoryBase<Role>, IRoleRepository
+    {
+
+        public EfRoleRepository(DbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
