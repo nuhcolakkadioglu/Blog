5c173b09c2b92c220c713ad2d2985aaf6080b74e
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2022-02-06 11:21:39 +0300

    Categorler Index eklendi.

---
+++ Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Results.ComplexType;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll();
+
+            if (result.ResultStatus == ResultStatus.Success)
+                return View(result.Data);
+
+            return View();
+        }
+    }
+}
